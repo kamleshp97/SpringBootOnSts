365f5e1d02fa3e560cb42ae67cb995a7ceeb3eb9
kamleshp97 <kamleshp097@gmail.com>
2023-09-21 16:28:40 +0530

    first commit

---
+++ src/main/java/com/spring/boot/SpringBootOnStsApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnStsApplication.class, args);
+		System.out.println("Project running");
+	}
+
+}
